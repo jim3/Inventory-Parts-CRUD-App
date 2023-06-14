4642b806297566287cbc5bfc3c74d9278721a2b6
jim3 <j3.casey@protonmail.com>
2023-06-14 10:56:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,6 @@ app.set("views", path.resolve(process.cwd(), "views"));
 // serve static assets from the public directory
 app.use(express.static(path.resolve(process.cwd(), "public")));
 
-// create a route for the index.html page in your views directory
-// use res.sendFile() but use ESM syntax to import the path module for the get route, ,cannot use __dirname
-// app.get("/", (req, res) => {
-//     res.sendFile(path.resolve(process.cwd(), "views", "index.html"));
-// });
-
 app.use("/", partsRoute);
 app.use("/", accountsRoute);
 
